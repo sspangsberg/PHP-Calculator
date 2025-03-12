839eadcaf7c4d35e220192762a391e2bba86ec19
Søren Spangsberg Jørgensen (Business Academy SouthWest) <smsj@easv.dk>
2025-03-12 16:39:21 +0100

    Update with AAA comments

--- tests/CalculatorControllerTest.php
+++ tests/CalculatorControllerTest.php
@@ -9,31 +9,42 @@ class CalculatorControllerTest extends TestCase
     private $calculator;
     private $controller;
 
+    /**
+     * Prepare any needed test dependencies
+     */
     public function setUp(): void
     {
         $this->calculator = new Calculator;
     }
    
+    /**
+     * Integration test - invalid input to calculator (add)
+     */
     public function testInvalidInputToHandleCalculation()
     {   
+        // Arrange
         $this->controller = new CalculatorController;
        
-        $result = $this->controller->handleCalculation("10asdf",20, "add");
-                
+        // Act
+        $result = $this->controller->handleCalculation("10asdf",20, "plus");
+              
+        // Assert
         $this->assertEquals("Numeric values are required", $result);
     }
 
+    /**
+     * Integration test - Valid input to calculator (add)
+     */
     public function testValidInputToHandleCalculation()
     {   
+        // Arrange
         $this->controller = new CalculatorController;
        
+        // Act
         $actual = $this->controller->handleCalculation(10,20, "plus");
-        $expected = "<h1>10 plus 20 equals 30</h1>";      
         
+        // Assert
+        $expected = "<h1>10 plus 20 equals 30</h1>";      
         $this->assertEquals($expected, $actual);
     }
 }
-
-
-
-
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -12,17 +12,21 @@ class CalculatorTest extends TestCase
         $this->calculator = new Calculator;
     }
 
+    /**
+     * 
+     */
     public function testAdd()
-    {  
-        $this->assertEquals(25, $this->calculator->add(20,5));
+    {
+        // AAA
+        $this->assertEquals(25, $this->calculator->add(20, 5));
     }
 
+    /**
+     * 
+     */
     public function testSubtract()
-    {   
-        $this->assertEquals(15, $this->calculator->subtract(20,5));
-    }   
+    {
+        // AAA
+        $this->assertEquals(15, $this->calculator->subtract(20, 5));
+    }
 }
-
-
-
-
